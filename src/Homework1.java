d0edc81ab4f6aeae052f19a6e5627b05397a1ddf
gronata <gromkorshni@gmail.com>
2020-09-01 16:13:54 +0300

    homework1

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Homework1 {
     // Ввести целое число и вывести на печать последнюю цифру
-    public static void main(String[] agrs){
+    public static void main(String[] agrs) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите целое число:");
         int num = input.nextInt();
-        System.out.printf("Последняя цифра : %d \n", num);
+        System.out.printf("Последняя цифра : %d \n", getCountsOfDigits(num));
         input.close();
+    }
+
+    public static int getCountsOfDigits(int number) {
+        return String.valueOf(Math.abs(number)).length();
     }
 }
