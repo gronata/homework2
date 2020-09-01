5350a5340cbb1b52c156a4ae1776e3a3fb83b2d5
gronata <gromkorshni@gmail.com>
2020-09-01 16:23:48 +0300

    homework1

--- src/Homework1.java
+++ src/Homework1.java
@@ -4,6 +4,7 @@ public class Homework1 {
     // Ввести целое число и вывести на печать последнюю цифру
     public static void main(String[] agrs) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Задание №1:");
         System.out.println("Введите целое число:");
         int num = input.nextInt();
         int lastCharacter = num%10;
